55701b71e64b7f21a24213f7a34ac2314435505a
Nicholas Werner <ncwerner@gmail.com>
2021-01-10 14:44:57 -0700

    removed gitignore files

---
+++ models/burger.js
@@ -0,0 +1,24 @@
+// import ORM
+const orm = require("../config/orm.js");
+
+// burger model
+const burger = {
+	select: function(cb) {
+		orm.selectAll(function(response) {
+			cb(response);
+		});
+	},
+	insert: function(burgerName, cb) {
+		orm.insertOne(burgerName, function(response) {
+			cb(response);
+		});
+	},
+	update: function(burgerStatus, burgerId, cb) {
+		orm.updateOne(burgerStatus, burgerId, function(response) {
+			cb(response);
+		});
+	}
+}
+
+// export burger model
+module.exports = burger;
